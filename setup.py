4ba2cf4ebf35b26d739038a2551cbe481b8251a9
sgreer77 <stephanie.greer@queensu.ca>
2018-09-06 14:25:43 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup 
+
+setup(
+        name = 'gemtools2',
+        packages = ['gemtools2'],
+        version = '0.2',
+        description = 'Tools to analyze 10X data',
+        author = 'Hanlee Ji lab',
+        author_email = 'sgreer2@stanford.edu',
+        url = 'https://github.com/sgreer77/gemtools2',
+        dowload_url = 'https://github.com/sgreer77/gemtools2/archive/0.2.tar.gz',
+        license = 'MIT',
+        #package_data = {'SVassembly': ['*.R']},#'*.Rscript']}, #added bc of R file
+        #include_package_data=True  #added bc of R file
+        #install_requires = ['pandas']  #need to fill this out
+        #keywords
+        #classifiers
+)
