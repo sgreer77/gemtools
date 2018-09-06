dcd75ed1beab9815724a7b58f03fc9f03d71942a
Stephanie <sgreer2@stanford.edu>
2018-09-06 16:17:34 -0700

    test by stephanie

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         url = 'https://github.com/sgreer77/gemtools2',
         dowload_url = 'https://github.com/sgreer77/gemtools2/archive/0.2.tar.gz',
         license = 'MIT',
+        entry_points={'console_scripts': ['gemtools2 = gemtools2.__main__:main']},
         #package_data = {'SVassembly': ['*.R']},#'*.Rscript']}, #added bc of R file
         #include_package_data=True  #added bc of R file
         #install_requires = ['pandas']  #need to fill this out
