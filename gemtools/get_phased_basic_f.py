000174825e18beeef418f4a32882d90b06e2d510
Stephanie <sgreer2@stanford.edu>
2018-10-04 10:53:52 -0700

    test by stephanie

--- gemtools/get_phased_basic_f.py
+++ gemtools/get_phased_basic_f.py
@@ -23,31 +23,63 @@ def get_phased_basic(inputvcf='None',outpre='out',**kwargs):
 
 	vcf_data=[]
 	for record in vcf_reader:
-		if record.is_snp and record.genotype(cur_sample)['GT'] in ["0|1","1|1","0/1","1/1","1|0","1/0"]:
-			format_field=(record.FORMAT).split(":")
-			if 'PS' in format_field:
-				block_id=record.genotype(cur_sample)['PS']
+		chr = record.CHROM
+		pos = record.POS
+		ref_allele = record.REF
+		alt_allele = record.ALT
+		geno = record.genotype(cur_sample)['GT']
+		
+		allele_list = list(ref_allele) + alt_allele
+		num_alts = len(alt_allele)
+		format_field=(record.FORMAT).split(":")
+		
+		if 'PS' in format_field:
+			block_id=record.genotype(cur_sample)['PS']
+		else:
+			block_id="n/a"
+
+		if '|' in geno:
+			phase_status = 'phased'
+			allele_1 = int(geno.split("|")[0])
+			allele_2 = int(geno.split("|")[1])
+		else:
+			phase_status = 'not_phased'
+			allele_1 = int(geno.split("/")[0])
+			allele_2 = int(geno.split("/")[1])
+			
+		if str(allele_1)==str(allele_2):
+			num_alleles = 1
+			hom_status = "hom"
+		else:
+			num_alleles = 2
+			hom_status = "het"
+		
+		if record.is_snp:
+			var_type = "snv"
+		else:
+			var_type = "indel"
+
+		if ('BX' in format_field and allele_1.isdigit() == True and allele_2.isdigit() == True):
+			bc1=record.genotype(cur_sample)['BX'][int(allele_1)]
+			bc2=record.genotype(cur_sample)['BX'][int(allele_2)]
+			if bc1=='':
+				bc1="n/a"	
+				bc1_ct=0
 			else:
-				block_id="n/a"
-			if 'BX' in format_field:
-				bc1=record.genotype(cur_sample)['BX'][0]
-				bc2=record.genotype(cur_sample)['BX'][1]
-				if bc1=='':
-					bc1="n/a"	
-					bc1_ct=0
-				else:
-					bc1_ct=len(bc1.split(";"))
-				if bc2=='':
-					bc2="n/a"
-					bc2_ct=0
-				else:
-					bc2_ct=len(bc2.split(";"))
-				if record.genotype(cur_sample)["GT"]=="1|0":
-					bc1,bc2=bc2,bc1
-					bc1_ct,bc2_ct=bc2_ct,bc1_ct
-			vcf_data.append((record.CHROM, record.POS, bc1_ct, bc1, bc2_ct, bc2, record.genotype(cur_sample)["GT"], block_id))
+				bc1_ct=len(bc1.split(";"))
+			if bc2=='':
+				bc2="n/a"
+				bc2_ct=0
+			else:
+				bc2_ct=len(bc2.split(";"))
+
+		else:
+			bc1,bc2,bc1_ct,bc2_ct="n/a"
+
+		vcf_data.append((chr,pos,ref_allele,alt_allele,geno,allele_list,num_alts,block_id,phase_status,allele_1,allele_2,num_alleles,hom_status,var_type,bc1,bc1_ct,bc2,bc2_ct))
 
 	df=pd.DataFrame(vcf_data)
+	df.columns=['chrom','pos','ref','alt','gt','allele_list','num_alts','block_id','phase_status','allele_1','allele_2','num_alleles','hom_status','var_type','bc1','bc1_ct','bc2','bc2_ct']
 
 	df.to_csv(str(outpre), sep="\t", index=False, header=False)
 
