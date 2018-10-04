cd3ae6a9ed1b27cf53bfe1e1de868fbfbd17b773
Stephanie <sgreer2@stanford.edu>
2018-10-04 15:45:19 -0700

    test by stephanie

--- gemtools/get_phased_basic_f.py
+++ gemtools/get_phased_basic_f.py
@@ -66,12 +66,14 @@ def parse_phase_blocks(r,s):
 	return [chr,pos,ref_allele,alt_allele,geno,allele_list,num_alts,block_id,phase_status,allele_1,allele_2,num_alleles,hom_status,var_type,bc1,bc1_ct,bc2,bc2_ct]
 
 
-def get_phased_basic(inputvcf='None',outpre='out',**kwargs):
+def get_phased_basic(inputvcf='None',outpre='out',chrom='None',**kwargs):
 
 	if 'vcf' in kwargs:
 		inputvcf = kwargs['vcf']
 	if 'out' in kwargs:
 		outpre = kwargs['out']
+	if 'chr' in kwargs:
+		chrom = kwargs['chr']
 
 	vcf_reader = vcf.Reader(open(inputvcf,'r'))
 
@@ -80,11 +82,18 @@ def get_phased_basic(inputvcf='None',outpre='out',**kwargs):
 
 	vcf_data=[]
 	
-	for record in vcf_reader:
-		parsed_record = parse_phase_blocks(record, cur_sample)
-		print parsed_record
-		vcf_data.append(parsed_record)
-		print vcf_data
+	if chrom=='None':
+		for record in vcf_reader:
+			parsed_record = parse_phase_blocks(record, cur_sample)
+			print len(parsed_record)
+			vcf_data.append(parsed_record)
+			print len(vcf_data)
+	else:
+		for record in vcf_reader.fetch(str(chrom)):
+			parsed_record = parse_phase_blocks(record, cur_sample)
+			print len(parsed_record)
+			vcf_data.append(parsed_record)
+			print len(vcf_data)
 
 	df=pd.DataFrame(vcf_data)
 	df.columns=['chrom','pos','ref','alt','gt','allele_list','num_alts','block_id','phase_status','allele_1','allele_2','num_alleles','hom_status','var_type','bc1','bc1_ct','bc2','bc2_ct']
