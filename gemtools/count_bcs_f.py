ccd062ed2710687ea49a4a2c1511ac2371ede3ac
Stephanie <sgreer2@stanford.edu>
2018-10-22 16:36:44 -0700

    test by stephanie

--- gemtools/count_bcs_f.py
+++ gemtools/count_bcs_f.py
@@ -84,7 +84,7 @@ def count_bcs(full_w_size=500000, small_w_size=1000,bc_subset='shared',sv_n="Non
 	df_list = []
 	for index,row in sv_df_full.iterrows():
 
-		df_name = str(row['name'] + "_df")
+		df_name = str(str(row['name']) + "_df")
 		# Create data frame of 1kb windows
 		start = int(row['w_start'])
 		stop = int(row['w_stop'])
