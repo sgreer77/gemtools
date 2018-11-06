9ded08e1cdd87405f295a398d99ac52c0c1bbe5b
Stephanie <sgreer2@stanford.edu>
2018-11-06 11:04:55 -0800

    test by stephanie

--- gemtools/refine_bcs_f.py
+++ gemtools/refine_bcs_f.py
@@ -23,8 +23,8 @@ def refine_bcs(**kwargs):
 
 	if 'bed_in' in kwargs:
 		bed_in = kwargs['bed_in']
-	if 'shared_in' in kwargs:
-		shared_in = kwargs['shared_in']
+	#if 'shared_in' in kwargs:
+	#	shared_in = kwargs['shared_in']
 	if 'bam' in kwargs:
 		bam_input = kwargs['bam']
 	if 'out' in kwargs:
@@ -32,12 +32,18 @@ def refine_bcs(**kwargs):
 
 	bam_open = pysam.Samfile(bam_input)
 
-	bed_df = pd.read_table(bed_in, sep="\t")
-	bed_df.columns = ['chrom','start','stop','name','status']
+	bed_df = pd.read_table(bed_in, sep="\t", comment="#", header=None, names=['chrom','start','stop','name','status'])
 	
 	bed_df['bcs'] = bed_df.apply(lambda row: get_barcode_ids(bam_open,str(row['chrom']),int(row['start']),int(row['end']),MIN_MAPQ),axis=1)
+	print bed_df
 	
 	bed_grouped = bed_df.groupby('name')
+	
+	for name, group in bed_grouped:
+		print name
+		print grouped
+	
+	sys.exit()
 	
 	for name, group in bed_grouped:
 	
@@ -49,7 +55,7 @@ def refine_bcs(**kwargs):
 			elif len(bc_list_in)>1:
 				common_bcs = set(bc_list_in[0])
 				for s in bc_list_in[1:]:
-				common_bcs.intersection_update(s)
+					common_bcs.intersection_update(s)
 
 		group_out = bed_grouped.loc[bed_grouped['status']=="out"]
 		if len(group_out.index)==0:
