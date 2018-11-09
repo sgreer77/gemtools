26d0837a68c488d0ebb9b6ccbb0a3137069e0146
Stephanie <sgreer2@stanford.edu>
2018-11-08 21:37:18 -0800

    test by stephanie

--- test/test_script.sh
+++ test/test_script.sh
@@ -12,25 +12,25 @@ echo "Testing get_shared_bcs..."
 gemtools -T get_shared_bcs -i svs.wndw.bedpe -b $BAM_FILE -o svs.shared.txt
 
 # Plotting
-echo "Testing count_bcs..."
-gemtools -T count_bcs -i svs.shared.txt -b $BAM_FILE -x 1000 -y 500000 -s 'call_2080,call_440,call_189' -q shared -o svs.bc_count.txt
-echo "Testing plot_hmw..."
-gemtools -T plot_hmw -i svs.bc_count.txt -o svs.pdf
+#echo "Testing count_bcs..."
+#gemtools -T count_bcs -i svs.shared.txt -b $BAM_FILE -x 1000 -y 500000 -s 'call_2080,call_440,call_189' -q shared -o svs.bc_count.txt
+#echo "Testing plot_hmw..."
+#gemtools -T plot_hmw -i svs.bc_count.txt -o svs.pdf
 
 # Assign haps with shared barcodes
-echo "Testing assign_sv_haps..."
-gemtools -T assign_sv_haps -i svs.shared.txt -v $VCF_FILE -o svs.haps.txt -w 1000000 -q shared
+#echo "Testing assign_sv_haps..."
+#gemtools -T assign_sv_haps -i svs.shared.txt -v $VCF_FILE -o svs.haps.txt -w 1000000 -q shared
 
 # Refine barcodes then rerun plotting
 echo "Testing refine_bcs..."
 gemtools -T refine_bcs -i $REFINED_FILE -b $BAM_FILE -o svs.shared_refined.txt -e svs.shared.txt
-echo "Testing count_bcs (again)..."
+echo "Testing count_bcs..."
 gemtools -T count_bcs -i svs.shared_refined.txt -b $BAM_FILE -x 1000 -y 500000 -s 'call_2080,call_440,call_189' -q select -o svs.bc_count_refined.txt
-echo "Testing plot_hmw (again)..."
+echo "Testing plot_hmw..."
 gemtools -T plot_hmw -i svs.bc_count_refined.txt -o svs_refined.pdf
 
 # Assign haps with select_barcodes -- better than before, with shared barcodes!
-echo "Testing assign_sv_haps (again)..."
+echo "Testing assign_sv_haps..."
 gemtools -T assign_sv_haps -i svs.shared_refined.txt -v $VCF_FILE -o svs.haps_refined.txt -w 1000000 -q select
 
 # Get phase block information
@@ -49,8 +49,8 @@ gemtools -T get_bcs_in_region -b $BAM_FILE -f 'chr9,128200000,128300000;chr9,128
 
 echo "Testing count_bcs_list..."
 gemtools -T count_bcs_list -b $BAM_FILE -f chr9,128200000,128300000 -x 1000 -l bcs_in_region.txt -o bc_count_from_list.txt
-echo "Testing plot_hmw (again)..."
-gemtools -T plot_hmw -i bc_count_from_list.txt -o list.pdf
+#echo "Testing plot_hmw (again)..."
+#gemtools -T plot_hmw -i bc_count_from_list.txt -o list.pdf
 
 # Subset fastq's
 echo "Testing extract_reads_interleaved..."
