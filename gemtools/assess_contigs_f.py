e9ea58d78ae3028bda88996e0915f753e101fd1f
sgreer77 <sgreer2@stanford.edu>
2019-02-13 14:03:01 -0800

    branch test by stephanie

--- gemtools/assess_contigs_f.py
+++ gemtools/assess_contigs_f.py
@@ -9,28 +9,25 @@ def assess_contigs(**kwargs):
 
 	df = pd.read_table(infile, sep="\t", index_col = 0)
 	df = df.reset_index() #otherwise 'read' column is index
-	grouped = df.groupby('read').filter(lambda x: len(x)>=2) #only reads that appear 2+ times
-	grouped = grouped.groupby('read')
-
-	df = pd.read_table(infile, sep="\t", index_col = 0)
 	df['interesting'] = False 
-    
+	#grouped = df.groupby('read').filter(lambda x: len(x)>=2) #only reads that appear 2+ times
+	grouped = df.groupby('read')
+
 	for name, group in grouped:
 		r_st_list = []
 		r_en_list = []
 		i = 0
-        	print name
+		
 		for row in group.iterrows():
-			print row[1]
-			print row 
-			if abs(row[1]['r_en'] - row[1]['r_st']) > 500: 
+			if abs(row[1]['r_en'] - row[1]['r_st']) > 500: # only consider mappings of at least 500
 				r_st_list.append(row[1]['r_st'])
 				r_en_list.append(row[1]['r_st'])
 				i += 1
-        
+        print name
+        print i
 		if i == 2: #should generally have 2 rows
 			if r_st_list[1] > r_en_list[0] or r_st_list[0] > r_en_list[1]: #interesting
 				df.loc[name, 'interesting'] = True
 
-	dfInteresting = df.query('interesting == True')
-	dfInteresting.to_csv(outfile, sep="\t")
+	df_interesting = df.query('interesting == True')
+	df_interesting.to_csv(outfile, sep="\t")
