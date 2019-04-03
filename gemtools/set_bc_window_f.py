f360564e28fb15ce4765ee01db893242d177eb4f
Stephanie <sgreer2@stanford.edu>
2019-04-03 15:07:26 -0700

    test by stephanie

--- gemtools/set_bc_window_f.py
+++ gemtools/set_bc_window_f.py
@@ -11,9 +11,10 @@ def set_bc_window(wsize=100000,**kwargs):
 	if 'bedpe' in kwargs:
 		sv_input = kwargs['bedpe']
 	if 'window' in kwargs:
-		wsize = kwargs['window']
-	else:
-		wsize = int(100000)
+		if 'window' is None:
+			wsize = int(100000)
+		else:
+			wsize = kwargs['window']
 	if 'out' in kwargs:
 		outpre = kwargs['out']
 	if 'mode' in kwargs:
