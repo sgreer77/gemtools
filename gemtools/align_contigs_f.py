313911f7deea24be3929c23b4c84226e1728b63b
sgreer77 <sgreer2@stanford.edu>
2019-06-18 10:27:40 -0700

    test by stephanie

--- gemtools/align_contigs_f.py
+++ gemtools/align_contigs_f.py
@@ -19,11 +19,11 @@ def align_contigs(**kwargs):
 
 	outfile = open(outfile, 'w')
 
-	outfile.write("read\tchr\tpos\tr_st\tr_en\tq_st\tq_en\tstrand\tcs\tcigstr\tcigtup\n")
+	outfile.write("read\tchr\tpos\tr_st\tr_en\tq_st\tq_en\tprimary\tstrand\tcs\tcigstr\tcigtup\n")
 
 	for name, seq, qual in mp.fastx_read(infile):
 		print name
 		for hit in a.map(seq, cs=True):
-			outfile.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(name, hit.ctg, hit.r_st, hit.r_st, hit.r_en, hit.q_st, hit.q_en, hit.strand, hit.cs, hit.cigar_str, hit.cigar))
+			outfile.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(name, hit.ctg, hit.r_st, hit.r_st, hit.r_en, hit.is_primary, hit.q_st, hit.q_en, hit.strand, hit.cs, hit.cigar_str, hit.cigar))
 
 	outfile.close()
