3b7479b0aba2d4fadcfb11a86aad60e290ebda41
Stephanie <sgreer2@stanford.edu>
2019-06-29 17:34:32 -0700

    test by stephanie

--- gemtools/plot_haps_and_blocks_f.py
+++ gemtools/plot_haps_and_blocks_f.py
@@ -82,8 +82,8 @@ def plot_haps_and_blocks(**kwargs):
 			axis(2,at=seq(1,4,1),labels=c("unphased","hap2","hap1","blocks"), cex.axis=1.25, mgp=c(3,0.7,0), las=1)
 
 			# plot variants
-			points(y_hap1~pos_mod, data=df_var[df_var$allele_1==1 & df_var$phase_status=='phased',], pch=4, cex=1.6, col="#009E73")
-			points(y_hap2~pos_mod, data=df_var[df_var$allele_2==1 & df_var$phase_status=='phased',], pch=4, cex=1.6, col="#E69F00")
+			points(y_hap1~pos_mod, data=df_var[df_var$allele_2==1 & df_var$phase_status=='phased',], pch=4, cex=1.6, col="#009E73")
+			points(y_hap2~pos_mod, data=df_var[df_var$allele_1==1 & df_var$phase_status=='phased',], pch=4, cex=1.6, col="#E69F00")
 			#points(y_hap1~pos_mod, data=df_var[df_var$allele_1==1 & df_var$phase_status=='phased' & df_var$pos==11468296,], pch=19, cex=1.6, col="red", bg="red")
 			points(y_unph~pos_mod, data=df_var[df_var$phase_status=='not_phased',], pch=4, cex=1.6)
 
